e01f27dfc700384dd872b114bae708b2f3a0f72a
Maximilian Heidenreich <github@maximilian-heidenreich.de>
2020-12-19 22:44:34 +0100

    📝 Added documentation

--- src/dsddb.ts
+++ src/dsddb.ts
@@ -6,7 +6,7 @@ import { exists } from "https://deno.land/std@0.81.0/fs/mod.ts";
  */
 export class DsDDB {
 
-    // =====================    VARS
+    // =====================    PROPS
 
     /**
      * The file path in which to store the data in.
@@ -21,44 +21,71 @@ export class DsDDB {
     /**
      * The actual data cache.
      */
-    cache: { [name: string]: string };
+    private cache: { [name: string]: string };
 
     /**
      * Stores the last loaded hash from store data.
      */
-    lastLoadStoreHash: string;
+    private lastLoadStoreHash: string;
 
 
     // =====================    CONSTRUCTOR
 
     /**
-     * TODO
-     * @param {string} storePath TODO
+     * Create a new {DsDDB} instance.
+     * 
+     * @param {string} storePath A custom path where to write data
      */
-    constructor(storePath: string) {
-        this.storePath = storePath;
+    constructor(storePath?: string) {
+
+        this.storePath = storePath ? storePath : `${new URL('.', Deno.mainModule).pathname}.dsddb/store.json`;
         this.decoder = new TextDecoder('utf-8');
         this.cache = { _hash: "" };
         this.lastLoadStoreHash = "";
+
     }
 
 
-    // =====================    ACCESS
+    // =====================    DATA ACCESS
 
+    /**
+     * Retrieves a value from database by specified key.
+     * 
+     * @param {string} key 
+     * @returns {string} Value
+     */
     public get(key: string): string {
         return this.cache[key];
     }
 
+    /**
+     * Set's a value in the database by the specified key.
+     * 
+     * @param {string} key 
+     * @param {string} value 
+     */
     public set(key: string, value: string) {
+
         this.cache[key] = value;
+
+        // Calculate hash (!Exclude _hash property).
         let values: { [name: string]: string } = {};
         Object.assign(values, this.cache);
         delete values["_hash"];
         let hash = createHash("md5");
         hash.update(JSON.stringify(values));
+
+        // Store new hash.
         this.cache["_hash"] = hash.toString();
+
     }
 
+    /**
+     * Check whether a key is stored inside the database.
+     * 
+     * @param {string} key Lookup key
+     * @returns {boolean} Whether the key is stored in the database
+     */
     public exists(key: string): boolean {
         return key in this.cache;
     }
@@ -66,21 +93,49 @@ export class DsDDB {
 
     // =====================    MANAGEMENT
 
-    public write(storePath?: string, force: boolean = false) {
+    /**
+     * Writes cached data to disk.
+     * Won't perform write if the last known hash from the store file 
+     * matches the current cache hash.
+     * 
+     * @param {string} storePath Custom file path used by write operation
+     * @param {boolean} force Ignore hashe comparison and force write
+     */
+    public async write(storePath?: string, force: boolean = false) {
+
+        // Write probably not necessary.
         if (!force && this.lastLoadStoreHash === this.cache["_hash"]) return;
+
         if (!storePath) storePath = this.storePath;
-        Deno.writeTextFile(storePath, JSON.stringify(this.cache));
+
+        // Write data.
+        await Deno.writeTextFile(storePath, JSON.stringify(this.cache));
+    
     }
 
-    public async load(storePath?: string) {
+    /**
+     * Load stored data from disk into cache.
+     * Won't update cache values if hash in store file matches current cache file.
+     * // TODO: Store & Check file hash.
+     * 
+     * @param {string} storePath Custom file path used by read operation
+     * @param {boolean} force Ignore hashe comparison and force read
+     */
+    public async load(storePath?: string, force: boolean = false) {
+
         if (!storePath) storePath = this.storePath;
         if (!await exists(storePath)) return;
+
+        // Load data from file.
         const data = await Deno.readFile(storePath);
         const decoded = JSON.parse(this.decoder.decode(data))
-        if (decoded._hash !== this.cache._hash) {
-            this.cache = decoded;
-            this.lastLoadStoreHash = decoded["_hash"];
-        }
+
+        // Reload probably not necessary.
+        if (!force && decoded._hash === this.cache._hash) return;
+        
+        // Store new data.
+        this.cache = decoded;
+        this.lastLoadStoreHash = decoded["_hash"];
         
     }
 
