13b7ffaeead70b0d5d90f056f160681855dbb7ef
Maximilian Heidenreich <contact@maximilian-heidenreich.de>
2022-12-07 13:04:39 +0100

    🔥 Removed legacy files

--- deps.ts
+++
@@ -1,1 +0,0 @@
-export { Md5 } from "https://deno.land/std@0.115.1/hash/md5.ts";
--- test_deps.ts
+++
@@ -1,4 +0,0 @@
-export {
-  assertExists,
-  assertThrows,
-} from "https://deno.land/std@0.115.1/testing/asserts.ts";
